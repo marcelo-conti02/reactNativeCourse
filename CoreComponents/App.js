3bc426345a28d8305bbb2ae1ef1a4efc69f978ff
marcelo <marceloconti9017@gmail.com>
2025-01-27 21:38:26 -0300

    Lesson 16

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,28 +1,22 @@
-import { View, StyleSheet, Text, Image, ImageBackground, ScrollView } from "react-native";
+import { View, StyleSheet, Text, Image, ImageBackground, ScrollView, ActivityIndicator, Button } from "react-native";
+import { useState } from "react";
 
 const logoImg = require("./assets/adaptive-icon.png");
 
+
+
 export default function App() {
+  const [showLoad, setShowLoad] = useState(true)
+
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Text><Text style={styles.text}>Hello </Text>word
-        </Text>
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <ImageBackground source={logoImg}>
-          <Text>iMAGE text</Text>
-        </ImageBackground>
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-        <Image style={styles.img} source={{ uri: "https://picsum.photos/300" }} />
-      </ScrollView>
+      <ActivityIndicator size="large" color="midnightblue" animating={showLoad} />
+      <Button title="Show Load" onPress={ () => {
+        if(showLoad == true)
+          setShowLoad(false)
+        else
+          setShowLoad(true)
+      }} />
     </View >
   );
 }
